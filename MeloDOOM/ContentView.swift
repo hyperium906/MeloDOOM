0f8dd63ad3bca57fa0c94187669c7b6ef8ba2602
hyperium906 <joshuadupati@gmail.com>
2026-03-28 09:45:51 -0400

    Initial Commit

---
+++ MeloDOOM/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MeloDOOM
+//
+//  Created by Joshua Dupati on 3/28/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MeloDOOM/MeloDOOMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeloDOOMApp.swift
+//  MeloDOOM
+//
+//  Created by Joshua Dupati on 3/28/26.
+//
+
+import SwiftUI
+
+@main
+struct MeloDOOMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
